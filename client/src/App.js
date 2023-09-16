9dc2601c91393474275149719c8e17ac521631ae
Melissa Solano <m.solanof13@gmail.com>
2023-09-15 22:58:35 -0600

    agregamos InfoReceta y mas

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import{Route, Routes} from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import NuevaReceta from './components/NuevaReceta';
+import InfoReceta from './components/InfoReceta';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Dashboard/>}/>
         <Route path='/recetas/nueva' element={<NuevaReceta/>}/>
+        <Route path='/receta/:id' element={<InfoReceta/>}/>
       </Routes>
     </div>
   );
--- server/controllers/recetas.controller.js
+++ server/controllers/recetas.controller.js
@@ -34,9 +34,18 @@ const borrarReceta = (req, res) =>{
 
 }
 
+const actualizarReceta = (req, res) => {
+    Recetas.findByIdAndUpdate({_id: req.params.id}, req.body, {new:true, runValidators:true})
+    .then(receta => res.json(receta))
+    .catch (err =>{
+        res.status(400).json(err);
+    })
+}
+
 module.exports={
     guardarReceta,
     buscarRecetas,
     buscarReceta,
-    borrarReceta
+    borrarReceta,
+    actualizarReceta
 }
